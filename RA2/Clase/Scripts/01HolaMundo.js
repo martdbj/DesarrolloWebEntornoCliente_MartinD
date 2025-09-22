94895c678006ccc67aba93d0cbce68c1bc2349e3
Martín Díaz Bastida <181728462+martdbj@users.noreply.github.com>
2025-09-22 14:25:54 +0200

    First exercises JavaScript of Variables and data types

---
+++ RA2/Clase/Scripts/01HolaMundo.js
@@ -0,0 +1,7 @@
+"use strict";
+let message = "Hola mundo";
+
+const COLOR_RED = "rgba(0, 204, 255, 1)";
+alert(message);
+
+
---
+++ RA2/Clase/Scripts/03TiposDatos.js
@@ -0,0 +1,10 @@
+let value;
+alert(typeof value);
+value = prompt("Inserta un número");
+
+alert(typeof value);
+
+alert(typeof +value);
+
+alert(Boolean(value > 0));
+
