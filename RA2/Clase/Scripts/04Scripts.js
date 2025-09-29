8ae4924f9653e9ffcddff7490259463054ba3ff5
Martín Díaz Bastida <181728462+martdbj@users.noreply.github.com>
2025-09-29 14:25:09 +0200

    Added exercises about ?? and ||

---
+++ RA2/Clase/Scripts/04Scripts.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let a = 4;
+
+switch (a) {
+    case 3:
+        alert("Muy pequeño");
+        break;
+    case 4:
+    case 5:
+        alert("Incorrecto!");
+        alert("Por qué no tomas clases de matemáticas");
+        break;
+    default:
+        alert("Desconozco estos valores");
+        break;
+
+}
