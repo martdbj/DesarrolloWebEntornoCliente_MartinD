84cf8b38897faf06fdde253b2eba5960355725e3
martdbj <181728462+martdbj@users.noreply.github.com>
2025-12-08 15:00:38 +0100

    Screen3 Terminado

--- PracticaRA4/script/Sala1.js
+++ PracticaRA4/script/Sala1.js
@@ -1,11 +1,13 @@
 "use strict";
+// Claves
 let clave = Symbol("clave")
+let nevera = Symbol("nevera")
 
 let cajaFuerte = {
     modelo: "SergioScript Max Ultra Seguridad",
     fabricacion: 1998,
 
-    nevera: {
+    [nevera]: {
         modelo: "bosch",
         clave: 1234,
         stock: ["Lechuga", "Muesli", "Platanos", "Tomates Podridos", "Fideos Instantaneos", "Chocolate"]
@@ -23,13 +25,16 @@ let screen1 = document.getElementById("screen1");
 let screen2 = document.getElementById("screen2");
 let screen3 = document.getElementById("screen3");
 
+// Constantes
 let MAX_ATTEMPS = 3;
+
 
 // Botones del juego
 const siguiente = document.getElementById("siguiente");
 const screen2Incorrecto = document.getElementsByClassName("screen2Incorrecto");
 const screen2Correcto = document.getElementById("screen2Correcto");
 const screen3Script = document.getElementById("screen3ScriptEnviar");
+const screen3Enviar = document.getElementById("screen3RespuestaEnviar");
 
 // Ocultar Screen1 y mostrar Screen2 
 siguiente.addEventListener("click", () => {
@@ -57,4 +62,20 @@ screen2Correcto.addEventListener("click", () => {
 // screen3 Comprobación comando introducido por el usuario
 screen3Script.addEventListener("click", () => {
     let inputUsuario = document.getElementById("inputListarObjeto").value.trim();
+    try {
+        let comandoIntroducido = eval(inputUsuario);
+        console.log(comandoIntroducido);
+    } catch (err) {
+        console.log("Tu comando ha lanzado un error");
+    }
 });
+
+screen3Enviar.addEventListener("click", () => {
+    let inputAnyo = document.getElementById("anyoFabricacion").value.trim();
+    if (inputAnyo == cajaFuerte["fabricacion"]) {
+        console.log("Correcto")
+    } else {
+        let mensajeError = document.getElementById("errorScreen3");
+        mensajeError.style.display = "block";
+    }
+})
